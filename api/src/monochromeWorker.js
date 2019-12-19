28a2658c5084642ff335db1148e10d6f8c2e4a45
Auggie <jhudak@alumni.berklee.edu>
2019-12-18 22:33:43 -0500

    mod-4-12

--- api/src/monochromeWorker.js
+++ api/src/monochromeWorker.js
@@ -3,5 +3,7 @@ const {workerData, parentPort} = require('worker_threads');
 
 gm(workerData.source)
     .monochrome()
-    .write(workerData.destination, () => {})
+    .write(workerData.destination, (error) => {
+      if (error) throw error;
+    })
 ;
