81819eb9dac83e06602f8b115a30415df3ea6622
Auggie <jhudak@alumni.berklee.edu>
2019-12-18 20:30:49 -0500

    mod-3-3

--- api/src/router.js
+++ api/src/router.js
@@ -1,8 +1,15 @@
 const {Router} = require('express');
+const multer = require('multer');
 
 const router = Router();
 
 const filename = (request, file, callback) => {
   callback(null, file.originalname);
 };
+
+const storage = multer.diskStorage({
+  destination: 'api/uploads/',
+  filename,
+});
+
 module.exports = router;
