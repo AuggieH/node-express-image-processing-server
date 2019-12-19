5a6a90ecfb8f17dc71eae1d4627dc92dc495995b
Auggie <jhudak@alumni.berklee.edu>
2019-12-18 20:57:48 -0500

    mod-3-6

--- api/app.js
+++ api/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/*', (request, response) => {
   response.sendFile(pathToIndex);
