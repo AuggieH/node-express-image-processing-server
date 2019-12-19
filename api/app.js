31e5c45742124b7b5928dae12b0fe0f3c216eaec
Auggie <jhudak@alumni.berklee.edu>
2019-12-18 21:35:53 -0500

    remove body-parser

--- api/app.js
+++ api/app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 const router = require('./src/router');
 
 const app = express();
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
 
-app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', router);
 
 app.use('/*', (request, response) => {
