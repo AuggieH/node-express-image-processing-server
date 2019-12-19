80c59318b5f483e48984b2692db71859f5f39097
Auggie <jhudak@alumni.berklee.edu>
2019-12-18 22:22:51 -0500

    mod-4-7

--- api/src/resizeWorker.js
+++ api/src/resizeWorker.js
@@ -3,5 +3,7 @@ const {parentPort, workerData} = require('worker_threads');
 
 gm(workerData.source)
     .resize(100, 100)
-    .write(workerData.destination, () => {})
+    .write(workerData.destination, (error) => {
+      if (error) throw error;
+    })
 ;
