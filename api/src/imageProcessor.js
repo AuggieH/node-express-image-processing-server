b0e9c8a827cbd64f1eed72c70108e416c4390c64
Auggie <jhudak@alumni.berklee.edu>
2020-01-02 20:00:44 -0500

    mod-5-2

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -2,8 +2,14 @@ const path = require('path');
 const {Worker, isMainThread} = require('worker_threads');
 
 const imageProcessor = () => {
-  return false;
+  return new Promise((resolve, reject) => {
+    if (isMainThread) {
+
+    } else {
+      reject(new Error('not on main thread'));
+    }
+    resolve();
+  });
 };
 
 module.exports = imageProcessor;
-;
