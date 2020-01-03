982d6ca4ec4b1af735402b4fedbcc52b69042d86
Auggie <jhudak@alumni.berklee.edu>
2020-01-02 20:07:35 -0500

    mod-5-5

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -4,6 +4,10 @@ const {Worker, isMainThread} = require('worker_threads');
 const pathToResizeWorker = path.resolve(__dirname, 'resizeWorker.js');
 const pathToMonochromeWorker = path.resolve(__dirname, 'monochromeWorker.js');
 
+const uploadPathResolver = (filename) => {
+  return path.resolve(__dirname, '../uploads', filename);
+};
+
 const imageProcessor = () => {
   return new Promise((resolve, reject) => {
     if (isMainThread) {
