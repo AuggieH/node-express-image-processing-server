0de69952d7c1158978054fed68c1018ccfabf64e
Auggie <jhudak@alumni.berklee.edu>
2020-01-02 20:10:40 -0500

    mod-5-6

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -8,7 +8,9 @@ const uploadPathResolver = (filename) => {
   return path.resolve(__dirname, '../uploads', filename);
 };
 
-const imageProcessor = () => {
+const imageProcessor = (filename) => {
+  const sourcePath = uploadPathResolver(filename);
+
   return new Promise((resolve, reject) => {
     if (isMainThread) {
 
